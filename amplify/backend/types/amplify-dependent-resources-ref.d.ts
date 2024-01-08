a1b0ec198eea09277960f286072e361192e78d7a
PenguGH <130692446+PenguGH@users.noreply.github.com>
2024-01-07 16:39:06 -0800

    added basic user authentication to our app and setup frontend and backend to be CI/CD with AWS Amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cs4990w24boardgametrbce295d7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
